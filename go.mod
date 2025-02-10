fc91682435a6d5909d82cad756403b401b97fe09
srg206 <you@example.com>
2025-02-10 13:02:17 +0300

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AI-IELTS-HSE/Monitoring-Lib
+
+go 1.23.0
---
+++ monitoring.go
@@ -0,0 +1,8 @@
+package monitoring
+
+import "fmt"
+
+// Hello выводит приветственное сообщение
+func Hello() {
+	fmt.Println("Hello from Monitoring Library!")
+}
